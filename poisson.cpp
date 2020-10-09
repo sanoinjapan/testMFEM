abf2de81e3a0ee7e4ed2c65cf87ea4d6008bb926
sanoinjapan <codytaku@gmail.com>
2020-10-09 11:33:02 +0900

    add somethig

--- poisson.cpp
+++ poisson.cpp
@@ -9,3 +9,20 @@
 using namespace std;
 using namespace mfem;
 
+int main(int argc, argv[])
+{
+    const char *mesh_file = "data/simple"; 
+    int order = 1;
+
+    double mat_val = 1.0;
+    double neumann_val = 0.0;
+    double dileclet_val = 1.0;
+
+
+    Mesh mesh(mesh_file, 1, 1);
+    int dim = mesh.Dimension();
+
+    cout << " dimension is " << dim << endl;
+
+
+}
