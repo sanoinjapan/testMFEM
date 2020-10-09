89087d2705fa5f62a21fa0ac7a356e0e02b31d1e
sanoinjapan <codytaku@gmail.com>
2020-10-09 11:07:15 +0900

    add Header

--- poisson.cpp
+++ poisson.cpp
@@ -1,2 +1,11 @@
 // This is for trainig of MFEM for myself
 // I gonna solve poisson equation by Discontinuous Galerkin method
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using namespace mfem;
+
